2ffd56e17ec04b1f07209079cdde19c3d2f3df8f
rubdelima <rubensndelima@hotmail.com>
2024-02-20 10:15:17 -0300

    todo-fix: fix background and mascote image in profile page

--- learn/lib/pages/profilePage.dart
+++ learn/lib/pages/profilePage.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import '../widgets/mascote.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Profile Page Content',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
+      child: MascoteWidget(),
     );
   }
 }
