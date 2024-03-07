9672563194c440dbaee2799b1d44f4b3af2818de
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-03-06 21:46:10 -0300

    Add Firebase authentication and core plugins

--- learn/lib/pages/parentsPages/loginParenstPage.dart
+++ learn/lib/pages/parentsPages/loginParenstPage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:learn/widgets/login/loginEnterButton.dart';
 import 'package:learn/widgets/login/loginAppBar.dart';
@@ -87,6 +88,21 @@ class _LoginParentsPageState extends State<LoginParentsPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  Future<void> signInWithFirebase() async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
   @override
   void dispose() {
     _emailController.dispose();
@@ -121,7 +137,8 @@ class _LoginParentsPageState extends State<LoginParentsPage> {
                   const SizedBox(height: 32),
                   LoginEnterButton(
                       onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/parentsMain');
+                        signInWithFirebase();
+                        // Navigator.pushReplacementNamed(context, '/parentsMain');
                         // Aqui você implementará a lógica de login.
                       },
                       title: "Entrar",
