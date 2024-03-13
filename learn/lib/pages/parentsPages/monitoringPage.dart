c0afa401a2c879d039afd136d7ca2cc4873f3b4e
acarlaguerra <acarlaguerra@gmail.com>
2024-03-13 15:19:11 -0300

    Adicionando Widget de Relatório de Desempenho na Página de Monitoramento

--- learn/lib/pages/parentsPages/monitoringPage.dart
+++ learn/lib/pages/parentsPages/monitoringPage.dart
@@ -82,7 +82,7 @@ class _MonitoringPageState extends State<MonitoringPage> {
                   .toList(),
             ],
           ),),
-          const SizedBox(height: 8,),
+          const SizedBox(height: 16,),
           const Text("XP da semana", style: TextStyle(fontFamily: "Fieldwork-Geo", color: Color(0xFF000000), fontWeight: FontWeight.bold,fontSize: 14),),
           const Text("Veja a quantidade de XP coletada ao longo da\nsemana pelos seus filhos.", style: TextStyle(fontFamily: "Fieldwork-Geo", color: Color(0xFF5c5c5c),fontSize: 12),),
           const SizedBox(height: 8,),
@@ -103,7 +103,31 @@ class _MonitoringPageState extends State<MonitoringPage> {
             children: widget.parent.dependents,
             selectedChild: selectedChild,
           ),
-          )
+          ),
+          const SizedBox(height: 16,),
+          const Text("Relatório de desempenho", style: TextStyle(fontFamily: "Fieldwork-Geo", color: Color(0xFF000000), fontWeight: FontWeight.bold,fontSize: 14),),
+          const Text("Gere um relatório de desempenho de todos os seus dependentes.", style: TextStyle(fontFamily: "Fieldwork-Geo", color: Color(0xFF5c5c5c),fontSize: 12),),
+          const SizedBox(height: 8,),
+          Container(
+            height: 119,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color:  const Color(0xFFA2A2A2).withOpacity(0.6),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow:  [BoxShadow(
+                color: Color(0xFF5C5C5C).withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 3,
+                offset:  Offset(1, 1),
+              )]
+            ),
+            child: Center(
+                  child: Image.asset(
+                "assets/images/appIcons/icon-lock-default.png",
+                height: 119 * 0.25)
+            )
+          ),
+          const SizedBox(height: 8,)
         ],
       ),
       ),)
